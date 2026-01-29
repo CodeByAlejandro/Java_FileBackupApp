00c7d8756a81b6006ad88d8addcef2f689f3e7a4
Alejandro De Groote <CodeByAlejandro@gmail.com>
2026-01-29 23:49:57 +0100

    Removed database_path column
    Removed database_path column from global_config table.

--- src/main/resources/db/migrations/V2__add_table_global_config.sql
+++ src/main/resources/db/migrations/V2__add_table_global_config.sql
@@ -3,7 +3,6 @@ CREATE TABLE global_config
     id                   INTEGER PRIMARY KEY,
 
     log_directory_path   TEXT    NOT NULL,
-    database_path        TEXT    NOT NULL,
     database_backup_path TEXT,
 
     created_at           INTEGER NOT NULL,
@@ -11,6 +10,5 @@ CREATE TABLE global_config
 
     CHECK (id = 1),
     CHECK (log_directory_path != ''),
-    CHECK (database_path != ''),
     CHECK (database_backup_path IS NULL OR database_backup_path != '')
 );
